2b3fe2e2f65d8fbf07748bc818ff6e71639bcc15
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-29 23:18:19 -0300

    feat: created tokens

---
+++ token/token.go
@@ -0,0 +1,55 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT  = "<"
+	GT  = ">"
+	LTE = "<="
+	GTE = ">="
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Delimiters
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
