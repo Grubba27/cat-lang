1d626c1b3be8ac5c753a6d9821bcbd55a9f2fff2
Gabriel Grubba <Grubba27@hotmail.com>
2022-12-01 08:04:20 -0300

    tests: added tests to boolean expressions

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -110,3 +110,134 @@ let result = add(five,ten);
 		}
 	}
 }
+func TestNextTokenBooleanOneTokenExpressions(t *testing.T) {
+
+	input := `!-/*5;
+5 < 10 > 5;
+
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	lexer := New(input)
+	for i, test := range tests {
+		tok := lexer.NextToken()
+		if tok.Type != test.expectedType {
+			t.Fatalf("Tests[%d] - tokentype wrong. expected=%q, got=%q", i, test.expectedType, tok.Type)
+		}
+		if tok.Literal != test.expectedLiteral {
+			t.Fatalf("Tests[%d] - literal wrong. expected=%q, got=%q", i, test.expectedLiteral, tok.Literal)
+		}
+	}
+}
+func TestNextTokenBooleanDoubleTokenExpressions(t *testing.T) {
+
+	input := `
+10 == 10;
+10 != 9;
+9 <= 12;
+12 >= 9;
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "9"},
+		{token.LTE, "<="},
+		{token.INT, "12"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "12"},
+		{token.GTE, "<="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+
+		{token.EOF, ""},
+	}
+
+	lexer := New(input)
+	for i, test := range tests {
+		tok := lexer.NextToken()
+		if tok.Type != test.expectedType {
+			t.Fatalf("Tests[%d] - tokentype wrong. expected=%q, got=%q", i, test.expectedType, tok.Type)
+		}
+		if tok.Literal != test.expectedLiteral {
+			t.Fatalf("Tests[%d] - literal wrong. expected=%q, got=%q", i, test.expectedLiteral, tok.Literal)
+		}
+	}
+}
+func TestNextTokenIFExpressions(t *testing.T) {
+
+	input := `
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+
+	lexer := New(input)
+	for i, test := range tests {
+		tok := lexer.NextToken()
+		if tok.Type != test.expectedType {
+			t.Fatalf("Tests[%d] - tokentype wrong. expected=%q, got=%q", i, test.expectedType, tok.Type)
+		}
+		if tok.Literal != test.expectedLiteral {
+			t.Fatalf("Tests[%d] - literal wrong. expected=%q, got=%q", i, test.expectedLiteral, tok.Literal)
+		}
+	}
+}
