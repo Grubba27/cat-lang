50ad3554a91894d9e29536b7d97bc1b618525680
Gabriel Grubba <Grubba27@hotmail.com>
2022-12-28 23:34:01 -0300

    chore: adjusted parser.go

--- parser/parser.go
+++ parser/parser.go
@@ -19,7 +19,35 @@ func New(lexer *lexer.Lexer) *Parser {
 	return p
 
 }
+func (p *Parser) currentTokenIs(t token.TokenType) bool {
+	return p.currentToken.Type == t
+}
+func (p *Parser) nextTokenIs(t token.TokenType) bool {
+	return p.nextToken.Type == t
+}
+func (p *Parser) expectNextToBe(t token.TokenType) bool {
+	if p.nextTokenIs(t) {
+		p.peekNext()
+		return true
+	}
+	return false
+}
 
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	statement := &ast.LetStatement{Token: p.currentToken}
+	if !p.expectNextToBe(token.IDENT) {
+		return nil
+	}
+	statement.Name = &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+	if !p.expectNextToBe(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.currentTokenIs(token.SEMICOLON) {
+		p.peekNext()
+	}
+	return statement
+}
 func (p *Parser) peekNext() {
 	p.currentToken = p.nextToken
 	p.nextToken = p.lexer.NextToken()
@@ -39,6 +67,7 @@ func (p *Parser) ParseProgram() *ast.Program {
 	program.Statements = []ast.Statement{}
 	for p.currentToken.Type != token.EOF {
 		statement := p.parseStatement()
+
 		if statement != nil {
 			program.Statements = append(program.Statements, statement)
 		}
